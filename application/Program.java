5d1b0b7eaf1fe9a1ee49f0e2474ead1492e4497f
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2024-10-16 12:01:45 -0300

    Add files via upload
    Alteração, adicionado pilares de POO e expressão this para referenciar aos atributos do Objeto

--- application/Program.java
+++ application/Program.java
@@ -1,32 +1,39 @@
-package application;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
-		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
-		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
-		System.out.printf("Triangle X area: %.4f%n", areaX);
-		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		if (areaX > areaY) {
-			System.out.println("Larger area: X");
-		} else {
-			System.out.println("Larger area: Y");
-		}
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        sc.close();
+    }
 }
