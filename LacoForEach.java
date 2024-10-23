474c817350e638a3068983a601644bb52d0895c3
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2024-10-23 16:41:43 -0300

    Add files via upload

---
+++ LacoForEach.java
@@ -0,0 +1,14 @@
+
+public class LacoForEach {
+
+    public static void main(String[] args) {
+
+        String vect[] = new String[] { "Maria", "João", "Luiza" };
+
+        for (String obj : vect) { // Aqui, você adiciona o tipo de dado, um nome para o laço e onde deseja buscar
+            System.out.println(obj);
+
+        }
+
+    }
+}
